57bfada68190ca4780e0a75e8a8f7f02337e96ee
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-04-09 17:45:20 +0800

    Update 4_11对象成员变量地址_类成员变量变量偏移值.cpp

--- 第4章数据语义学/DataSematics/4_11对象成员变量地址_类成员变量变量偏移值.cpp
+++ 第4章数据语义学/DataSematics/4_11对象成员变量地址_类成员变量变量偏移值.cpp
@@ -14,17 +14,17 @@ public:
 
 void myfunc(int MYACLS::*mempoint, MYACLS &obj)
 {
-	obj.*mempoint = 260; //ע��д��
+	obj.*mempoint = 260; //注意写法
 }
 
 int main()
 {
-	//һ�������Ա�����ڴ��ַ����ָ��_�Ǹ�������ָ��
+	//一：对象成员变量内存地址及其指针_是个真正的指针
 	MYACLS myobj;
 	myobj.m_i = myobj.m_j = myobj.m_k = 0;
-	printf("myobj.m_i = %p\n", &myobj.m_i); //����ĳ�Ա���������������ڴ��ַ�ġ�
+	printf("myobj.m_i = %p\n", &myobj.m_i); //对象的成员变量是有真正的内存地址的。因此可通过指针操作内存。
 
-	//���Ϸ����ڴ�
+	//堆上分配内存
 	MYACLS *pmyobj = new MYACLS();
 	printf("pmyobj->m_i = %p\n", &pmyobj->m_i);
 	printf("pmyobj->m_j = %p\n", &pmyobj->m_j);
@@ -35,36 +35,36 @@ int main()
 	*p1 = 15;
 	*p2 = 30;
 
-	printf("p1��ַ=%p,p1ֵ=%d\n", p1, *p1);
-	printf("p2��ַ=%p,p2ֵ=%d\n", p2, *p2);
+	printf("p1地址=%p,p1值=%d\n", p1, *p1);
+	printf("p2地址=%p,p2值=%d\n", p2, *p2);
 	/*
 	 myobj.m_i = 004FF8C0
 	 pmyobj->m_i = 0070E438
 	 pmyobj->m_j = 0070E43C
-	 p1��ַ=004FF8C0,p1ֵ=15
-	 p2��ַ=0070E43C,p2ֵ=30
+	 p1地址=004FF8C0,p1值=15
+	 p2地址=0070E43C,p2值=30
 	 */
 
-	//������Ա������ƫ��ֵ����ָ�루�;��������û�й�ϵ��--���Ƕ����
-	cout << "��ӡ��Ա����ƫ��ֵ----------------" << endl;
-	printf("MYACLS::m_iƫ��ֵ = %d\n", &MYACLS::m_i); //��ӡƫ��ֵ�������õ�%d
-	printf("MYACLS::m_jƫ��ֵ = %d\n", &MYACLS::m_j);
-	//�ó�Ա����ָ������ӡƫ��ֵҲ���ԣ���д��
-	//���Ҫ֪������Ա����ָ����߱���� ʵ�����Ǹ�ƫ��ֵ�����Ǹ�ʵ���ڴ��ַ����
-	int MYACLS::*mypoint = &MYACLS::m_j;
-	printf("MYACLS::m_jƫ�Ƶ�ַ = %d\n", mypoint);
+	//二：成员变量的偏移值及其指针（和具体对象是没有关系的--不是对象绑定
+	cout << "打印成员变量偏移值----------------" << endl;
+	printf("MYACLS::m_i偏移值 = %d\n", &MYACLS::m_i); //打印偏移值，这里用的%d
+	printf("MYACLS::m_j偏移值 = %d\n", &MYACLS::m_j);
+	//用成员变量指针来打印偏移值也可以，看写法
+	//大家要知道，成员变量指针里边保存的 实际上是个偏移值（不是个实际内存地址）。
+	int MYACLS::*mypoint(得加上类名，表示是类内部的) = &MYACLS::m_j;
+	printf("MYACLS::m_j偏移地址 = %d\n", mypoint);
 
-	mypoint = &MYACLS::m_i; //����ע�⣬����ʹ��ʱֱ�������֣�����ʱ����Ҫ��MYACLS::
-	printf("MYACLS::m_iƫ�Ƶ�ַ = %d\n", mypoint);
+	mypoint = &MYACLS::m_i; //这里注意，单独使用时直接用名字，定义时才需要加MYACLS::
+	printf("MYACLS::m_i偏移地址 = %d\n", mypoint);
 	/*
-	 MYACLS::m_iƫ��ֵ = 0
-	MYACLS::m_jƫ��ֵ = 4
-	MYACLS::m_jƫ�Ƶ�ַ = 4
-	MYACLS::m_iƫ�Ƶ�ַ = 0
+	 MYACLS::m_i偏移值 = 0
+	MYACLS::m_j偏移值 = 4
+	MYACLS::m_j偏移地址 = 4
+	MYACLS::m_i偏移地址 = 0
 	 */
 
-	//����û��ָ���κ����ݳ�Ա������ָ��
-	//ͨ�� һ�����������߶���ָ���߸�  ��Ա����ָ�� ������ĳ������ĳ�Ա������
+	//三：没有指向任何数据成员变量的指针
+	//通过 一个对象名或者对象指针后边跟  成员变量指针 来访问某个对象的成员变量：
 	cout << "****************************" << endl;
 	myobj.m_i = 13;
 	myobj.*mypoint = 22;
@@ -72,30 +72,30 @@ int main()
 
 	myfunc(mypoint, myobj);
 	myfunc(mypoint, *pmyobj);
-	cout << "sizeof(mypoint) =" << sizeof(mypoint) << endl; //��Ա����ָ��Ҳ�Ǹ�4�ֽڣ�
+	cout << "sizeof(mypoint) =" << sizeof(mypoint) << endl; //成员变量指针也是个4字节；
 
-	//���ۣ���Ա����ָ��ָ���Ա�����׵�ַ����000000000���㸳ֵΪ0������������1����Ϊ0xffff ffff
+	//结论：成员变量指针指向成员变量首地址才是000000000，你赋值为0则编译器给你减1，变为0xffff ffff
 	int *ptest = 0;
 	int MYACLS::*mypoint2;
-	mypoint2 = 0; //��Ա����ָ�븳ֵΪ0�������������1
+	mypoint2 = 0; //成员变量指针赋值为0，编译器给你减1(防止你成员变量为0，表示指向成员变量的开头位置)(-1表示无效的偏移量)
 	mypoint2 = NULL; //0xffffffff
 	printf("mypoint2 = %d\n", mypoint2);//-1
 
-	//if(mypoint2 == mypoint) //��������һ����0��һ����-1
+	//if(mypoint2 == mypoint) //不成立，一个是0，一个是-1
 	int MYACLS::*mypoint10 = &MYACLS::m_i;
-	if (mypoint == mypoint10) //������
+	if (mypoint == mypoint10) //成立的
 	{
-		cout << "����" << endl;
+		cout << "成立" << endl;
 	}
 
-	//mypoint2 += 1;//���������ü�
+	//mypoint2 += 1;//编译器不让给成员变量指针加减
 	//mypoint2++;
 	//mypoint2 = ((&MYACLS::m_i) + 1);
 
 	/*
 	 * sizeof(mypoint) =4
 		mypoint2 = -1
-		����
+		成立
 	 */
 
 	system("pause");
