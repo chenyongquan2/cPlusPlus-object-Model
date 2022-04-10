4fa06b18b88012b8193f4048181f4e6992ebed33
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-04-10 13:07:00 +0800

    Update 5_10_1指向成员函数的指针.cpp

--- 第5章函数语义学/FunctionSemantics/5_10_1指向成员函数的指针.cpp
+++ 第5章函数语义学/FunctionSemantics/5_10_1指向成员函数的指针.cpp
@@ -7,13 +7,13 @@ using namespace std;
 
 
 	/*
-	 *һ��ָ���Ա������ָ��
-	 *��Ա������ַ������ʱ��ȷ���õġ����ǣ����ó�Ա����������Ҫͨ�����������õģ�
-	 *���г��棨�Ǿ�̬����Ա������Ҫ����ã�����Ҫ һ����������������
+	 *一：指向成员函数的指针
+	 *成员函数地址，编译时就确定好的。但是，调用成员函数，是需要通过对象来调用(因为需要传递this指针)的；
+	 *所有常规（非静态）成员函数，要想调用，都需要 一个对象来调用它；
 	 *
-	 *���Ա�����Ǳ����ڴ�����������ľ�̬��Ա�������ඨ��ʱ���Ѿ���ȫ���������������ڴ棬
-	 �������������ġ����ڷǾ�̬��Ա�����������������ʵ����������(�������)����ջ�����߶���Ϊ������ڴ棬
-	��Ϊÿ����������һ�������������������ڶ���ġ�
+	 *类成员函数是被放在代码区，而类的静态成员变量在类定义时就已经在全局数据区分配了内存，
+	 因而它是属于类的。对于非静态成员变量，我们是在类的实例化过程中(构造对象)才在栈区或者堆区为其分配内存，
+	是为每个对象生成一个拷贝，所以它是属于对象的。
 	 */
 
 class A
@@ -30,30 +30,30 @@ public:
 
 	static void mysfunc(int tempvalue)
 	{
-		cout << "A::mysfunc()��̬��Ա����--tempvalue = " << tempvalue << endl;
+		cout << "A::mysfunc()静态成员函数--tempvalue = " << tempvalue << endl;
 	}
 };
 
 void func()
 {
 	A mya;
-	void (A::*pmypoint)(int) = &A::myfunc1; //����һ����Ա����ָ�벢����ֵ
-	pmypoint = &A::myfunc2; //����Ա����ָ�븳ֵ
+	void (A::*pmypoint)(int) = &A::myfunc1; //定义一个成员函数指针并给初值
+	pmypoint = &A::myfunc2; //给成员函数指针赋值
 
-	(mya.*pmypoint)(15); //ͨ����Ա����ָ�������ó�Ա����������Ҫͨ������Ľ�����ܵ���
+	(mya.*pmypoint)(15); //通过成员函数指针来调用成员函数，必须要通过对象的介入才能调用
 
 	A *pmya = new A();
-	(pmya->*pmypoint)(20); //�ö���ָ�������ʹ�ó�Ա����ָ�� �����ó�Ա����
+	(pmya->*pmypoint)(20); //用对象指针介入来使用成员函数指针 来调用成员函数
 
-   //�������ӽ�
+   //编译器视角
    //pmypoint(&mya, 15);
    //pmypoint(pmya, 20);
 
-	//��̬����ָ�������ͨ�ĺ���ָ��
-	void(*pmyspoint)(int) = &A::mysfunc; //һ����ͨ�ĺ���ָ�룬�����ǳ�Ա����ָ��
+	//静态函数指针就是普通的函数指针
+	void(*pmyspoint)(int) = &A::mysfunc; //一个普通的函数指针，而不是成员函数指针
 	pmyspoint(80);
 
-	//ͨ����Ա����ָ��Գ���ĳ�Ա�������õĳɱ�����ͨ����ͨ�ĺ���ָ�������þ�̬��Ա�������ɱ��ϲ�ࣻ
+	//通过成员函数指针对常规的成员函数调用的成本，和通过普通的函数指针来调用静态成员函数，成本上差不多；
 
 
 }
@@ -70,8 +70,8 @@ int main()
 /*
     tempvalue2 = 15
 	tempvalue2 = 20
-	A::mysfunc()��̬��Ա����--tempvalue = 80
-	�밴���������. . .
+	A::mysfunc()静态成员函数--tempvalue = 80
+	请按任意键继续. . .
 
  */
 
