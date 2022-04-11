c53a90b242736299cdcfe1fc21e1c5762c2e2e5c
chenyongquan2 <101924391+chenyongquan2@users.noreply.github.com>
2022-04-11 13:23:52 +0800

    Update 6_4局部静态对象的构造和析构.cpp

--- 第6章对象构造语义学/TheSemanticsOfConstructor/6_4局部静态对象的构造和析构.cpp
+++ 第6章对象构造语义学/TheSemanticsOfConstructor/6_4局部静态对象的构造和析构.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 /*
- * һ���ֲ���̬����Ĺ��������
- *	a)������ǲ�����myfunc()��������ô�������ᴥ��A�Ĺ��캯����
- *	b)�ֲ���̬�����ڴ��ַ���ڱ����ڼ��ȷ���õģ�
- *	c)��̬�ֲ����տ�ʼҲ����ʼ��Ϊ0��
- *	d)�ֲ���̬���������������main����ִ�н�����ű����õġ���ǰ���������̬�ֲ����󱻹������
+ * 一：局部静态对象的构造和析构
+ *	a)如果我们不调用myfunc()函数，那么根本不会触发A的构造函数；
+ *	b)局部静态对象，内存地址是在编译期间就确定好的(只不过不会去触发调用构造函数而已。)；
+ *	c)静态局部量刚开始也被初始化为0；
+ *	d)局部静态对象的析构，是在main函数执行结束后才被调用的。（前提是这个静态局部对象被构造过）
  *
- *���ۣ� �ֲ���̬����ֻ�ᱻ����һ�Σ��ڵ��õ�ʱ���죻��main����ִ����Ϻ�����
+ *结论： 局部静态对象只会被构造一次，在调用的时候构造；在main函数执行完毕后析构
  *
  *	
  */
@@ -35,11 +35,11 @@ public:
 //void myfunc()
 const A &myfunc()
 {
-	//�ֲ���̬����
-	static A s_aobj1; //����myfunc()���������ü��Σ�s_aobj1���־�̬�ֲ�����ֻ�ᱻ����1�Σ�ֻ����һ�ι��캯����������ͨ������һ��������ж��Ƿ��������
+	//局部静态对象
+	static A s_aobj1; //不管myfunc()函数被调用几次，s_aobj1这种静态局部对象只会被构造1次（只调用一次构造函数）编译器通过设置一个标记来判断是否构造过对象
 	//static A s_aobj2;
-	printf("s_aobj1�ĵ�ַ��%p\n", &s_aobj1);
-	//printf("s_aobj2�ĵ�ַ��%p\n", &s_aobj2);
+	printf("s_aobj1的地址是%p\n", &s_aobj1);
+	//printf("s_aobj2的地址是%p\n", &s_aobj2);
 	return s_aobj1;
 }
 
@@ -59,9 +59,9 @@ int main()
 
 /*
 A::A()
-s_aobj1�ĵ�ַ��008AD14C
-s_aobj1�ĵ�ַ��008AD14C
-�밴���������. . .
+s_aobj1的地址是008AD14C
+s_aobj1的地址是008AD14C
+请按任意键继续. . .
 
  */
 
